d5bfdbf8dcdca8dd0b613ab363e1266d3258c638
EstebanErb <125437969+EstebanErb@users.noreply.github.com>
2024-04-22 09:22:56 +0200

    Add files via upload

---
+++ JDOP/Site 19_04/Catalogue.php
@@ -0,0 +1,228 @@
+<?php
+
+$servername = "185.98.131.109";
+$username = "lejar1343688";
+$password = "PHPljdop2016!";
+$dbname = "lejar1343688";
+
+
+try {
+    $pdo = new PDO('mysql:host=' . $servername . ';dbname=' . $dbname, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $error) {
+    echo $error;
+}
+
+include "Bandeau.html";
+
+$query = "SELECT * FROM plante WHERE idPlante > 0";
+$query2 = "SELECT DISTINCT TypePlante FROM plante";
+$query4 = "SELECT DISTINCT TypeFeuillage FROM plante";
+
+if (isset($_POST['TypePlante'])) {
+    $destination = $_POST['TypePlante'];
+    $query .= " AND TypePlante like '%$destination%'";
+}
+
+if (isset($_POST['Exposition'])) {
+    $Expo = $_POST['Exposition'];
+    $query .= " AND Exposition like '%$Expo%'";
+}
+
+if (isset($_POST['hauteur'])) {
+    $Hauteur = $_POST['hauteur'];
+    $query .= " AND Hauteur like '%$Hauteur%'";
+}
+
+if (isset($_POST['TypeFeuillage'])) {
+    $Feuillage = $_POST['TypeFeuillage'];
+    $query .= " AND TypeFeuillage like '$Feuillage%'";
+}
+$query .= " ORDER BY NomPlante";
+$stmt = $pdo->prepare("$query");
+if ($stmt-> execute()) {
+    $plante = $stmt -> fetchAll();
+    
+}
+
+$stmt2 = $pdo->prepare("$query2");
+if ($stmt2-> execute()) {
+    $filtre = $stmt2 -> fetchAll();
+    
+}
+
+$stmt4 = $pdo->prepare("$query4");
+if ($stmt4 -> execute()) {
+    $Feuillage = $stmt4 -> fetchAll();
+    
+}
+
+
+
+?>
+    <form action="Catalogue.php" method="POST">
+            <div class="SearchTravel">
+
+                <h1>Parcourez le catalogue</h1>
+
+                <select name="TypePlante" type="text" style="height:36px;" value="">
+                    <option value= > séléctionnez votre type de plante </option>
+					<?php foreach ($filtre as $p) { ?>
+							<option value=<?php echo $p["TypePlante"]?>> <?php echo $p["TypePlante"]?> </option>
+					<?php } ?>                    
+                </select>
+
+                <select name="Exposition" type="text" style="height:36px;" value="">
+                    <option value= > séléctionnez votre exposition </option>
+					<option value="soleil"> soleil </option>
+                    <option value="mi-ombre"> mi-ombre </option>
+                    <option value=" ombre "> ombre </option>
+                </select>
+
+                <select name="TypeFeuillage" type="text" style="height:36px;" >
+                    <option value= > séléctionnez votre type de feuillage </option>
+					<option value="caduc" > caduc </option>
+					<option value="semi-pers"> semi-persistant</option>
+                    <option value="pers"> persistant </option>        
+                </select>
+
+                <div class="Search2">
+                    <button type="submit" style="height:30px;">Recherche<i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </div>
+        </form>
+
+
+
+    <?php foreach($plante as $p){ ?>
+        <table> 
+            <tbody>
+                    <tr>
+                        <th>
+                            <!--Type : <?php echo $p["TypePlante"]  ?><br>-->
+                            <strong><?php echo $p["NomLatin"] ?></strong><br>
+                            <strong><?php echo $p["NomPlante"] ?></strong><br>
+                            <br>
+                            <?php if(isset($p["CouleurFleur"])){ ?>
+                                <strong>Couleur de la fleur : </strong><?php echo $p["CouleurFleur"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+
+                            <?php if(isset($p["CouleurFeuillage"])){ ?>
+                                <strong>Couleur du feuillage : </strong><?php echo $p["CouleurFeuillage"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+
+<!--                            <?php if(isset($p["Hauteur"])){ ?>
+                                <strong>Hauteur : </strong><?php echo $p["Hauteur"] ?> m <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+
+                            <?php if(isset($p["Largeur"])){ ?>
+                                <strong>Largeur : </strong><?php echo $p["Largeur"] ?> m <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>-->
+                            <?php if(isset($p["Floraison"])){ ?>
+                                <strong>Floraison : </strong><?php echo $p["Floraison"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+                            
+                            <?php if(isset($p["TypeFeuillage"])){ ?>
+                                <strong>Type de feuillage: </strong><?php echo $p["TypeFeuillage"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+
+                            <?php if(isset($p["Exposition"])){ ?>
+                                <strong>Exposition: </strong><?php echo $p["Exposition"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+
+                            <?php if(isset($p["TypeSol"])){ ?>
+                                <strong>Type de sol : </strong><?php echo $p["TypeSol"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+                            
+                            <?php if(isset($p["Silhouette"])){ ?>
+                                <strong>Silhouette : </strong><?php echo $p["Silhouette"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+                            
+                            <?php if(isset($p["Densite"])){ ?>
+                                <strong>Densité : </strong><?php echo $p["Densite"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+                            
+                            <?php if(isset($p["Rusticite"])){ ?>
+                                <strong>Rusticite : </strong><?php echo $p["Rusticite"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+                            
+                            <?php if(isset($p["Utilisation"])){ ?>
+                                <strong>Utilisation : </strong><?php echo $p["Utilisation"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+                            
+                            <?php if(isset($p["Entretien"])){ ?>
+                                <strong>Entretien : </strong><?php echo $p["Entretien"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+                            
+                            <?php if(isset($p["PetitMoins"])){ ?>
+                                <strong>Petit - : </strong><?php echo $p["PetitMoins"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+                            
+                            <?php if(isset($p["PetitPlus"])){ ?>
+                                <strong>Petit + : </strong><?php echo $p["PetitPlus"] ?> <br> <?php ;
+                                    }
+                                    else{
+                                        echo "";
+                                    } ?>
+
+                        </th>
+                        <th>
+                            <?php if(isset($p["imgPlante"])){ ?>
+                                    <img src=" <?php echo $p["imgPlante"] ?>" width="350"> 
+                                    <?php }
+                                    else{
+                                        echo "La valeur n'existe pas";
+                                    } ?><br>
+                            <strong><?php echo $p["NomLatin"] ?></strong><br>
+                            <strong><?php echo $p["NomPlante"] ?></strong>
+                        </th>
+                    </tr>
+            </tbody>
+        </table>
+    <?php } ?>
+
+    </body>
+
+</html>
