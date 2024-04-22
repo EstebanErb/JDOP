c022aad627dfb506785652c9119575074f315053
EstebanErb <125437969+EstebanErb@users.noreply.github.com>
2024-04-22 09:26:59 +0200

    Add files via upload

---
+++ Site 19_04/commandeeeeee.php
@@ -0,0 +1,21 @@
+<?php
+
+include "Bandeau.html";
+?>
+	<html>
+	</div>
+		</br>
+		<p>Tout n'est pas disponible en même temps.</p>
+		<p>En hiver on installe les arbustes et les arbres, fruitiers et ornementaux.</p>
+		<p>A printemps et à l'automne c'est une bonne période pour mettre en place les vivaces, que ce soient des fleurs ou des aromatiques.</p>
+		<p>Dès que les gelées ne sont plus à craindre (début mai ou après les Saints de Glace selon ses habitudes) on installe les légumes d'été (tomates, poivrons…) et les annuelles qui craignent le froid (zinnia, cosmos...).</p>
+		<p>Fin d'été on n'oublie pas les choux et les poireaux pour l'hiver !</p>
+		<p>Le bon de commande pour 2024 est en cours d'élaboration et bientôt en ligne !</p>	
+		<p>Si vous avez commandé des plants, voici le <a class="link" href="Tableau_abreviations.pdf">tableau</a> qui récapitule les abréviations présentes sur les godets</p>
+
+	</body>
+	<?php 
+		include "footer.html"
+	?>
+
+</html>
